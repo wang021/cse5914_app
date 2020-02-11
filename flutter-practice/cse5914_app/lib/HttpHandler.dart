497561a76dae6521924d992a0e20286d1d7ddb9b
Jiaqi Wang <wangjiaqi981021@gmail.com>
2020-02-11 14:57:46 -0500

    Add HttpHandler class and implement text to audio function. By YuKun Feng

---
+++ flutter-practice/cse5914_app/lib/HttpHandler.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
+
+//@app.route("/speech2text"):
+//@app.route("/getanswer"):
+class HttpHandler{
+  AudioPlayer player = AudioPlayer();
+
+  String basicAuthorizationHeader(String apikey) {
+    return 'Basic ' + base64Encode(utf8.encode('apikey:$apikey'));
+  }
+
+  String T2S_url = "https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/1a9e6446-ef2f-492b-b8ac-1867bd1739c4/v1/synthesize";
+  String T2S_key = "u9WLXjNk3bcPkXWtVPTiIokDCdVkVrY-R1SB_fPBtGdE";
+
+ void _text2audio(String text) async { 
+
+    Response response = await Dio().request(
+      T2S_url,
+      data: {'text': text, 'accept': 'audio/flac'},
+      options: Options(method: "POST", responseType: ResponseType.bytes, headers: {'Authorization': basicAuthorizationHeader(T2S_key)})
+    );
+
+    Directory tmpDir = await getTemporaryDirectory();
+    File tmpFile = File('${tmpDir.path}/T2S.flac');
+
+    await tmpFile.writeAsBytes(response.data, flush: true);
+    player.play('${tmpDir.path}/T2S.flac', isLocal: true);
+
+  }
+}
